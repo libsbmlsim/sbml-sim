9911e6e72c0ea04c5afdd78e4c462ea163e42d1c
Pranav Ballaney <ballaneypranav@gmail.com>
2021-06-11 19:34:10 +0530

    Can't figure out how to transfer &mut refs

--- src/main.rs
+++ src/main.rs
@@ -6,102 +6,112 @@ use xml;
 use xml::reader::{EventReader, XmlEvent};
 
 mod structs;
-use structs::model::Model;
-use structs::model::TagIndex;
-use structs::math::MathExp;
+//use structs::math::MathExp;
+use structs::model::*;
+//use structs::model::TagIndex;
 
-mod helpers;
-use helpers::parse_expression;
-use helpers::print_postfix;
+//mod helpers;
+//use helpers::parse_expression;
+//use helpers::print_postfix;
 
 fn main() {
-  // read cmd line args
-  let args: Vec<String> = env::args().collect();
-  let filename = &args[1];
+    // read cmd line args
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
 
-  // read file
-  let file = File::open(filename).unwrap();
-  let file = BufReader::new(file);
-  let parser = EventReader::new(file);
+    // read file
+    let file = File::open(filename).unwrap();
+    let file = BufReader::new(file);
+    let parser = EventReader::new(file);
 
-  let mut stack: Vec<TagIndex> = Vec::new();
-  let mut model = Model::new();
-  let mut current: TagIndex = model.add_node(String::from("root"));
+    //let mut stack: Vec<TagIndex> = Vec::new();
+    let mut root = Model::new();
+    let mut current = Refs::Model(&mut root);
 
-  for e in parser {
-    match e {
-      // for each starting tag
-      Ok(XmlEvent::StartElement {
-        name, attributes, ..
-      }) => {
-        // read tag
-        let tag = model.add_node(String::from(name.local_name));
-        // read attributes
-        for attribute in attributes {
-          model.add_attr(tag, attribute.name.local_name, attribute.value);
+    for e in parser {
+        match e {
+            // for each starting tag
+            Ok(XmlEvent::StartElement {
+                name, attributes, ..
+            }) => {
+                match name.local_name.as_str() {
+                    "listOfSpecies" => match current {
+                        Refs::Model(ref mut ptr) => {
+                            ptr.list_of_species = Some(ListOfSpecies::new().with_parent(current));
+                            current = Refs::ListOfSpecies(&mut ptr.list_of_species.unwrap());
+                        }
+                        _ => {}
+                    },
+                    _ => {}
+                }
+                // read tag
+                //let tag = model.add_node(String::from(name.local_name));
+                //// read attributes
+                //for attribute in attributes {
+                //model.add_attr(tag, attribute.name.local_name, attribute.value);
+                //}
+                //// append to current tag and advance
+                //model.add_child(current, tag);
+                //current = tag;
+                //// push to stack
+                //stack.push(tag);
+            }
+            // for each closing tag
+            Ok(XmlEvent::EndElement { name }) => {
+                // read tag name
+                //let tag = name.local_name;
+                //// if this is the last tag in the stack
+                //if *model.get_tag_name(current) == tag {
+                //// pop out and advance
+                //if stack.len() > 1 {
+                //stack.pop();
+                //current = stack[stack.len() - 1];
+                //} else if stack.len() > 0 {
+                //stack.pop();
+                //}
+                //}
+            }
+            // read text within tags
+            Ok(XmlEvent::Characters(s)) => {
+                //model.add_text(current, String::from(s.trim()));
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+            _ => {}
         }
-        // append to current tag and advance
-        model.add_child(current, tag);
-        current = tag;
-        // push to stack
-        stack.push(tag);
-      }
-      // for each closing tag
-      Ok(XmlEvent::EndElement { name }) => {
-        // read tag name
-        let tag = name.local_name;
-        // if this is the last tag in the stack
-        if *model.get_tag_name(current) == tag {
-          // pop out and advance
-          if stack.len() > 1 {
-            stack.pop();
-            current = stack[stack.len() - 1];
-          } else if stack.len() > 0 {
-            stack.pop();
-          }
-        }
-      }
-      // read text within tags
-      Ok(XmlEvent::Characters(s)) => {
-        model.add_text(current, String::from(s.trim()));
-      }
-      Err(e) => {
-        println!("Error: {}", e);
-      }
-      _ => {}
     }
-  }
 
-  model.root = current;
-  let root = model.root;
+    //model.root = current;
+    //let root = model.root;
 
-  // print species IDs
-  println!("Species IDs: ");
-  let results = helpers::find(&model, Some(root), String::from("species"));
-  for result in results {
-    print!("{}  ", model.tags[result].attributes.get("id").unwrap());
-  }
-  println!();
+    //// print species IDs
+    //println!("Species IDs: ");
+    //let results = helpers::find(&model, Some(root), String::from("species"));
+    //for result in results {
+    //print!("{}  ", model.tags[result].attributes.get("id").unwrap());
+    //}
+    //println!();
 
-  // find kinetic laws
-  let kinetic_laws = helpers::find(&model, Some(root), String::from("kineticLaw"));
-  // find math nodes
-  let mut expressions: Vec<MathExp> = Vec::new();
-  for law in kinetic_laws {
-    let math_nodes = helpers::find(&model, Some(law), String::from("math"));
-    // parse expressions and store
-    for expression in math_nodes {
-      let mut parsed_expr = MathExp::new();
-      parse_expression(&model, expression, &mut parsed_expr);
-      expressions.push(parsed_expr);
-    }
-  }
+    //// find kinetic laws
+    //let kinetic_laws = helpers::find(&model, Some(root), String::from("kineticLaw"));
+    //// find math nodes
+    //let mut expressions: Vec<MathExp> = Vec::new();
+    //for law in kinetic_laws {
+    //let math_nodes = helpers::find(&model, Some(law), String::from("math"));
+    //// parse expressions and store
+    //for expression in math_nodes {
+    //let mut parsed_expr = MathExp::new();
+    //parse_expression(&model, expression, &mut parsed_expr);
+    //expressions.push(parsed_expr);
+    //}
+    //}
 
-  println!();
-  // print expressions
-  println!("Math nodes in Reverse Polish notation:");
-  for expression in expressions {
-    print_postfix(&expression, expression.root);
-    println!();
-  }
+    //println!();
+    //// print expressions
+    //println!("Math nodes in Reverse Polish notation:");
+    //for expression in expressions {
+    //print_postfix(&expression, expression.root);
+    //println!();
+    //}
 }
--- src/structs/model.rs
+++ src/structs/model.rs
@@ -1,60 +1,113 @@
-use std::collections::HashMap;
+//use std::collections::HashMap;
 
 // An SBML Model container
 #[derive(Debug)]
-pub struct Model {
-  pub tags: Vec<Tag>,
-  pub root: TagIndex,
+pub struct Model<'a> {
+    pub name: Option<String>,
+    pub list_of_species: Option<ListOfSpecies<'a>>,
+    //pub tags: Vec<Tag>,
+    //pub root: TagIndex,
 }
-
-pub type TagIndex = usize;
 
 #[derive(Debug)]
-pub struct Tag {
-  pub tag: String,
-  pub text: String,
-  pub attributes: HashMap<String, String>,
-  pub children: Vec<TagIndex>,
+pub struct ListOfSpecies<'a> {
+    pub species: Vec<Species<'a>>,
+    pub parent: Option<ListOfSpeciesParent<'a>>,
 }
 
-impl Model {
-  // returns a new SBML model
-  pub fn new() -> Model {
-    return Model {
-      tags: Vec::new(),
-      root: 0,
-    };
-  }
-
-  // adds a node to the model and returns its index
-  pub fn add_node(&mut self, tag: String) -> TagIndex {
-    let index = self.tags.len();
-    self.tags.push(Tag {
-      tag: tag,
-      text: String::from(""),
-      attributes: HashMap::new(),
-      children: Vec::new(),
-    });
-    return index;
-  }
-
-  pub fn add_text(&mut self, tag: TagIndex, text: String) {
-    self.tags[tag].text = text;
-  }
-
-  pub fn add_attr(&mut self, tag: TagIndex, attr: String, value: String) {
-    self.tags[tag].attributes.insert(attr, value);
-  }
-
-  pub fn add_child(&mut self, source: TagIndex, target: TagIndex) {
-    self.tags[source].children.push(target);
-  }
-  
-  pub fn get_tag_name(&self, index: TagIndex) -> &String {
-    return &self.tags[index].tag;
-  }
-
-  pub fn get_text(&self, index: TagIndex) -> &String {
-    return &self.tags[index].text;
-  }
+#[derive(Debug)]
+pub enum ListOfSpeciesParent<'a> {
+    Model(&'a Model<'a>),
 }
+
+#[derive(Debug)]
+pub struct Species<'a> {
+    pub id: String,
+    pub name: Option<String>,
+    pub parent: Option<SpeciesParent<'a>>,
+}
+
+#[derive(Debug)]
+pub enum SpeciesParent<'a> {
+    ListOfSpecies(&'a ListOfSpecies<'a>),
+}
+#[derive(Debug)]
+pub enum Refs<'a> {
+    Model(&'a mut Model<'a>),
+    ListOfSpecies(&'a ListOfSpecies<'a>),
+    Species(&'a Species<'a>),
+}
+
+//pub type TagIndex = usize;
+
+//#[derive(Debug)]
+//pub struct Tag {
+//pub tag: String,
+//pub text: String,
+//pub attributes: HashMap<String, String>,
+//pub children: Vec<TagIndex>,
+//}
+
+impl Model<'_> {
+    // returns a new SBML model
+    pub fn new() -> Model<'static> {
+        return Model {
+            name: None,
+            list_of_species: None,
+            //tags: Vec::new(),
+            //root: 0,
+        };
+    }
+}
+
+impl<'a> ListOfSpecies<'a> {
+    pub fn new() -> ListOfSpecies<'static> {
+        return ListOfSpecies {
+            species: Vec::new(),
+            parent: None,
+        };
+    }
+
+    pub fn with_parent(mut self, parent: Refs<'a>) -> Self {
+        match parent {
+            Refs::Model(p) => {
+                self.parent = Some(ListOfSpeciesParent::Model(p));
+            }
+            _ => {}
+        }
+        self
+    }
+}
+
+// adds a node to the model and returns its index
+//pub fn add_node(&mut self, tag: String) -> TagIndex {
+//let index = self.tags.len();
+//self.tags.push(Tag {
+//tag: tag,
+//text: String::from(""),
+//attributes: HashMap::new(),
+//children: Vec::new(),
+//});
+//return index;
+//}
+
+//pub fn add_text(&mut self, tag: TagIndex, text: String) {
+//self.tags[tag].text = text;
+//}
+
+//pub fn add_attr(&mut self, tag: TagIndex, attr: String, value: String) {
+//self.tags[tag].attributes.insert(attr, value);
+//}
+
+//pub fn add_child(&mut self, source: TagIndex, target: TagIndex) {
+//self.tags[source].children.push(target);
+//}
+
+//pub fn get_tag_name(&self, index: TagIndex) -> &String {
+//return &self.tags[index].tag;
+//}
+
+//pub fn get_text(&self, index: TagIndex) -> &String {
+//return &self.tags[index].text;
+//}
+//}
